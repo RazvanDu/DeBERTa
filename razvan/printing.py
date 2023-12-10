234b8834097bdf4c7b98bb55f56b203f44d0b6a7
RazvanDu <razvandumm@gmail.com>
2023-12-10 09:24:09 +0200

    Final code.

--- razvan/printing.py
+++ razvan/printing.py
@@ -1,4 +1,4 @@
-with open('deberta_embd.tsv', 'r', encoding='utf-8') as file:
+with open('deberta_embd_saved.tsv', 'r', encoding='utf-8') as file:
     file_content = file.read()
     print('Read from file:', file_content)
     print('Hexadecimal:', file_content.encode('utf-8').hex())
